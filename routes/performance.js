4c4e67dbbb4dbb00a7750f3b2c12b49e20c63c48
taehoonkang <sls97117@gmail.com>
2022-07-29 01:28:47 +0900

    feat: 공연 라우터

--- routes/performance.js
+++ routes/performance.js
@@ -2,12 +2,45 @@ const express = require(`express`)
 
 const { Performance } = require(`../models`)
 
+const { isLoggedIn } = require(`./middlewares`)
 const router = express.Router()
 
 // 티켓 생성
 
-// 티켓 상세 정보 보기
+router.post(`/`, isLoggedIn, async (req, res, next) => {
+    try{
+        const performance = await Performance.create({
+            title: req.body.title,
+            place: req.body.place,
+            time: req.body.time,
+            term_start_at: req.body.term_start_at,
+            term_end_at: req.body.term_end_at,
+            start_at: req.body.start_at,
+            end_at: req.body.end_at,
+            description: req.body.description,
+            img_src: req.body.img_src
+        })
+        res.status(200).json(performance)
+    } catch(err){
+        console.error(err)
+        next(err)
+    }
+} )
+// 공연 상세 정보 보기
 
-
+router.get(`/:id`,  async (req, res, next) => {
+    try{
+        const performance = await Performance.findOne({
+            where: { id: req.params.id}
+        })
+        if(!performance){
+            return res.status(403).send(`존재하지 않는 공연입니다.`)
+        }
+        res.status(201).json(performance)
+    } catch(err){
+        console.error(err)
+        next(err)
+    }
+} )
 
 module.exports = router
---
+++ routes/performances.js
@@ -0,0 +1,24 @@
+const express = require(`express`)
+
+const { Performance } = require(`../models`)
+
+
+const router = express.Router()
+
+// 모든 공연 정보 보기
+
+router.get(`/`,  async (req, res, next) => {
+    try{
+        const performance = await Performance.findAll({
+        })
+        if(!performance){
+            return res.status(403).send(`현재는 공연이 없습니다.`)
+        }
+        res.status(201).json(performance)
+    } catch(err){
+        console.error(err)
+        next(err)
+    }
+} )
+
+module.exports = router
