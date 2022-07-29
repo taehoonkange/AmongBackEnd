639158431f6c5be59183198a71dd4da402cb7b5e
taehoonkang <sls97117@gmail.com>
2022-07-29 20:57:26 +0900

    feat: 쿠키 파서 수정

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(cors({
 app.use(`/`, express.static(path.join(__dirname, `uploads`)))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser())
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
     saveUninitialized: false,
     resave: false,
