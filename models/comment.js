08387eb0367445f53f6bfeec522323ea74f976ea
taehoonkang <sls97117@gmail.com>
2022-08-07 17:47:17 +0900

    feat: Comment 모델 추가

---
+++ models/comment.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+    const Comment = sequelize.define(`Comment`, {
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        nickname: {
+            type: DataTypes.STRING(15),
+            allowNull: false
+        },
+
+    }, {
+        modelName: 'Comment',
+        tableName: 'comments',
+        charset: `utf8mb4`,
+        collate: `utf8mb4_general_ci`
+    });
+    Comment.associate = (db) => {
+        db.Comment.belongsTo(db.User)
+        db.Comment.belongsTo(db.Board)
+    }
+
+    return Comment;
+}
