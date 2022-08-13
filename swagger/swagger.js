37271dc6dd5e9a5b2c065806c5a8798a80084c3a
taehoonkang <sls97117@gmail.com>
2022-08-13 14:24:23 +0900

    fix: swagger 수정

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -30,6 +30,20 @@ const doc = {
     ],
     host: "localhost:3065",
     schemes: ["http"],
+    definitions: {
+        Performance: {
+            title: "공연 제목 입력",
+            place: "공연 장소 입력",
+            time: "공연 시간",
+            term_start_at: "YYYY-MM-DD HH:MM:SS",
+            term_end_at: "YYYY-MM-DD HH:MM:SS",
+            start_at: "YYYY-MM-DD HH:MM:SS",
+            end_at: "YYYY-MM-DD HH:MM:SS",
+            description: "공연 상세 설명",
+            infos: [{class: "좌석 등급", price: "좌석 가격", number: "좌석 갯수"},],
+            allow_resale: "true or false"
+        }
+    }
 };
 
 const outputFile = "./swagger-output.json";
