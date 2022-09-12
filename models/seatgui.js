fb895cf333dd976f880c1eabebc6b0bb66afd227
taehoonkang <sls97117@gmail.com>
2022-09-12 14:34:44 +0900

    feat: seatgui 모델 추가

---
+++ models/seatgui.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, DataTypes) => {
+    const Seatgui = sequelize.define(`Seatgui`, {
+
+        seatNumber:{
+            type: DataTypes.INTEGER,
+        },
+        x:{
+            type: DataTypes.INTEGER,
+        },
+        y:{
+            type: DataTypes.INTEGER,
+        },
+        status: {
+            type: DataTypes.STRING(8),
+            allowNull: true
+        },
+        color: {
+            type: DataTypes.STRING(9),
+            allowNull: true
+        },
+
+    }, {
+        modelName: 'Seatgui',
+        tableName: 'seatguies',
+        charset: `utf8mb4`,
+        collate: `utf8mb4_general_ci`
+    });
+    Seatgui.associate = (db) => {
+        db.Seatgui.belongsTo(db.Performance) // 어느 공연인지
+    }
+
+    return Seatgui;
+}
